4210e2d682e544f597d2db6598d319b0f08405e9
K. Duncan <kkduncan@cse.usf.edu>
2014-06-19 13:41:42 -0400

    Added ReadME

---
+++ Release/OPESettings.d
@@ -0,0 +1,5 @@
+OPESettings.d: ../OPESettings.cpp ../OPESettings.h ../OPECommon.h
+
+../OPESettings.h:
+
+../OPECommon.h:
---
+++ Release/objects.mk
@@ -0,0 +1,8 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+USER_OBJS :=
+
+LIBS := -lboost_thread -lboost_system -lpcl_search -lvtkCommon -lvtkFiltering -lvtkHybrid -lvtkRendering -lpcl_common -lpcl_io -lpcl_kdtree -lpcl_filters -lpcl_sample_consensus -lpcl_segmentation -lpcl_visualization -lpcl_features -lpcl_surface
+
